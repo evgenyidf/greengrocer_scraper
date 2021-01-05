5f3873c34279ec706edb6a34af788c9fff68545d
Evgeny Subotin <evgeny.subotin@cyren.com>
2021-01-05 10:50:57 +0200

    Adding noyhasade to be scraped

--- scrape.py
+++ scrape.py
@@ -91,19 +91,19 @@ URLS = {
         # 'ילדודס':        'https://www.carmella.co.il/product-category/%d7%99%d7%9c%d7%93%d7%95%d7%93%d7%a1/',
         # 'משקאות':       'https://www.carmella.co.il/product-category/%d7%9e%d7%a9%d7%a7%d7%90%d7%95%d7%aa/',
         # 'פרחים':        'https://www.carmella.co.il/product-category/%d7%a4%d7%a8%d7%97%d7%99%d7%9d/'
+    },
+    'noyhasade': {
+        'פירות': 'https://noyhasade.co.il/product-category/%d7%a4%d7%99%d7%a8%d7%95%d7%aa/%d7%a4%d7%99%d7%a8%d7%95%d7%aa-%d7%98%d7%a8%d7%99%d7%99%d7%9d/',
+        'ירקות גינה': 'https://noyhasade.co.il/product-category/%d7%99%d7%a8%d7%a7%d7%95%d7%aa/%d7%99%d7%a8%d7%a7%d7%95%d7%aa-%d7%92%d7%99%d7%a0%d7%94/',
+        'חסות': 'https://noyhasade.co.il/product-category/%d7%99%d7%a8%d7%a7%d7%95%d7%aa/%d7%97%d7%a1%d7%95%d7%aa-%d7%a0%d7%91%d7%98%d7%99%d7%9d-%d7%a2%d7%9c%d7%99%d7%9d/',
+        'עשבי-תיבול': 'https://noyhasade.co.il/product-category/%d7%99%d7%a8%d7%a7%d7%95%d7%aa/%d7%a2%d7%a9%d7%91%d7%99-%d7%aa%d7%99%d7%91%d7%95%d7%9c/',
+        'עשבי-תיבול': 'https://noyhasade.co.il/product-category/%d7%99%d7%a8%d7%a7%d7%95%d7%aa/%d7%a2%d7%a9%d7%91%d7%99-%d7%aa%d7%99%d7%91%d7%95%d7%9c/',
+        'פתריות': 'https://noyhasade.co.il/product-category/%d7%99%d7%a8%d7%a7%d7%95%d7%aa/%d7%a4%d7%98%d7%a8%d7%99%d7%95%d7%aa/',
+        'ביצים': 'https://noyhasade.co.il/product-category/%d7%9e%d7%a2%d7%93%d7%a0%d7%99%d7%94/%d7%91%d7%99%d7%a6%d7%99%d7%9d-%d7%97%d7%9c%d7%91-%d7%95%d7%92%d7%91%d7%99%d7%a0%d7%95%d7%aa/',
+        'דבש': 'https://noyhasade.co.il/product-category/%d7%9e%d7%a2%d7%93%d7%a0%d7%99%d7%94/%d7%98%d7%97%d7%99%d7%a0%d7%94-%d7%97%d7%9c%d7%91%d7%94-%d7%a1%d7%99%d7%9c%d7%90%d7%9f-%d7%95%d7%93%d7%91%d7%a9/',
+        'פיצוחים': 'https://noyhasade.co.il/product-category/%d7%94%d7%a7%d7%95%d7%9c%d7%99%d7%a0%d7%a8%d7%99%d7%94-%d7%a9%d7%9c-%d7%a0%d7%95%d7%99/%d7%a4%d7%99%d7%a6%d7%95%d7%97%d7%99%d7%9d/',
+        'תבלינים': 'https://noyhasade.co.il/product-category/%d7%94%d7%a7%d7%95%d7%9c%d7%99%d7%a0%d7%a8%d7%99%d7%94-%d7%a9%d7%9c-%d7%a0%d7%95%d7%99/%d7%aa%d7%91%d7%9c%d7%99%d7%a0%d7%99%d7%9d/'
     }
-    # 'noyhasade': {
-    #     'פירות': 'https://noyhasade.co.il/product-category/%d7%a4%d7%99%d7%a8%d7%95%d7%aa/%d7%a4%d7%99%d7%a8%d7%95%d7%aa-%d7%98%d7%a8%d7%99%d7%99%d7%9d/',
-    #     'ירקות גינה': 'https://noyhasade.co.il/product-category/%d7%99%d7%a8%d7%a7%d7%95%d7%aa/%d7%99%d7%a8%d7%a7%d7%95%d7%aa-%d7%92%d7%99%d7%a0%d7%94/',
-    #     'חסות': 'https://noyhasade.co.il/product-category/%d7%99%d7%a8%d7%a7%d7%95%d7%aa/%d7%97%d7%a1%d7%95%d7%aa-%d7%a0%d7%91%d7%98%d7%99%d7%9d-%d7%a2%d7%9c%d7%99%d7%9d/',
-    #     'עשבי-תיבול': 'https://noyhasade.co.il/product-category/%d7%99%d7%a8%d7%a7%d7%95%d7%aa/%d7%a2%d7%a9%d7%91%d7%99-%d7%aa%d7%99%d7%91%d7%95%d7%9c/',
-    #     'עשבי-תיבול': 'https://noyhasade.co.il/product-category/%d7%99%d7%a8%d7%a7%d7%95%d7%aa/%d7%a2%d7%a9%d7%91%d7%99-%d7%aa%d7%99%d7%91%d7%95%d7%9c/',
-    #     'פתריות': 'https://noyhasade.co.il/product-category/%d7%99%d7%a8%d7%a7%d7%95%d7%aa/%d7%a4%d7%98%d7%a8%d7%99%d7%95%d7%aa/',
-    #     'ביצים': 'https://noyhasade.co.il/product-category/%d7%9e%d7%a2%d7%93%d7%a0%d7%99%d7%94/%d7%91%d7%99%d7%a6%d7%99%d7%9d-%d7%97%d7%9c%d7%91-%d7%95%d7%92%d7%91%d7%99%d7%a0%d7%95%d7%aa/',
-    #     'דבש': 'https://noyhasade.co.il/product-category/%d7%9e%d7%a2%d7%93%d7%a0%d7%99%d7%94/%d7%98%d7%97%d7%99%d7%a0%d7%94-%d7%97%d7%9c%d7%91%d7%94-%d7%a1%d7%99%d7%9c%d7%90%d7%9f-%d7%95%d7%93%d7%91%d7%a9/',
-    #     'פיצוחים': 'https://noyhasade.co.il/product-category/%d7%94%d7%a7%d7%95%d7%9c%d7%99%d7%a0%d7%a8%d7%99%d7%94-%d7%a9%d7%9c-%d7%a0%d7%95%d7%99/%d7%a4%d7%99%d7%a6%d7%95%d7%97%d7%99%d7%9d/',
-    #     'תבלינים': 'https://noyhasade.co.il/product-category/%d7%94%d7%a7%d7%95%d7%9c%d7%99%d7%a0%d7%a8%d7%99%d7%94-%d7%a9%d7%9c-%d7%a0%d7%95%d7%99/%d7%aa%d7%91%d7%9c%d7%99%d7%a0%d7%99%d7%9d/'
-    # }
 
 }
 
